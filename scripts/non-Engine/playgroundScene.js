c896f915d83d8bf350919472cfd8f7d0a8ae618e
Nick Pepper <nick.pepper@db-n.com>
2023-01-25 17:44:27 +0100

    first commit

---
+++ scripts/non-Engine/playgroundScene.js
@@ -0,0 +1,25 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+
+}
+
+function create ()
+{
+}
+
+function update ()
+{
+}
