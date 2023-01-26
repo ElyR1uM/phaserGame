4642089e8fbadf26039c2416867729463214cb09
Nick Pepper <nick.pepper@db-n.com>
2023-01-26 15:20:51 +0100

    Redid most of the code to finally work

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
-import titleScene from './src/scenes/titleScene';
-
-const { Phaser } = require("./dist/phaser");
-
-let titleScene = new titleScene();
 let config = {
     type: Phaser.WEBGL,
     backgroundColor: '#2dab2d',
     width: 1920,
-    height: 1080
+    height: 1080,
+    pixelArt: true,
 };
 
+class titleSceneClass extends Phaser.Scene {
+    constructor() {
+        super({key: 'titleScene'});
+    }
+
+    preload() {
+        this.load.image('bg', './assets/exported-images/backgroundCave.png');
+    }
+
+    create () {
+        let background = this.add.image(0, 0, 'bg').setOrigin(0);
+        background.setScale(7.5)
+    }
+}
+let titleScene = new titleSceneClass();
 let game = new Phaser.Game(config);
 game.scene.add('titleScene', titleScene);
 game.scene.start('titleScene')
