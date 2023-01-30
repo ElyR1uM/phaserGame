b742b9bae07a416b2e1f32142015a85ca355e5b0
Nick Pepper <nick.pepper@db-n.com>
2023-01-30 16:56:29 +0100

    Added working example

--- main.js
+++ main.js
@@ -1,32 +1,38 @@
-let config = {
-    type: Phaser.WEBGL,
-    backgroundColor: 'rgb(11, 16, 30)',
-    width: 2560,
-    height: 1440,
-    pixelArt: true, // Definetly never delete this, Upscaling an image so much with pixel interpolation enabled costs resources and looks horrifying
-};
 
 class titleSceneClass extends Phaser.Scene {
     constructor() {
         super({key: 'titleScene'});
     }
 
-    preload() {
-        this.load.image('bg', './assets/exported-images/backgroundCave.png'); // For further calculations relative to the game resolution: original image dimensions are 256x144 (10th of 2K)
-        this.load.image('Planks', './assets/exported-images/cPathTiles.png'); //Load resources for the tilemap JSON (Definetly load before dbnF1.json)
-        this.load.tilemapTiledJSON('map', './assets/exported-tilemaps/dbnF1.json'); //Load the tilemap
+    preload () {
+        this.load.image('bg', 'assets/exported-images/backgroundCave.png'); // For further calculations relative to the game resolution: original image dimensions are 256x144 (10th of 2K)
+        this.load.image('tiles', 'assets/exported-images/cPathTiles.png'); //Load resources for the tilemap JSON (Definetly load before dbnF1.json)
+        this.load.tilemapTiledJSON('map', 'assets/exported-tilemaps/dbnF1.json'); //Load the tilemap
     }
 
     create () {
         //let background = this.add.image(0, 0, 'bg').setOrigin(0);
         //background.setScale(10); // If the resolution is set to 1920x1080px (FHD) then setScale(7.5), if set to 2560x1440 (2K QHD) then setScale(10)
 
-        let groundFloor = this.make.tilemap({ key: 'map' });
-
-        let terrain = groundFloor.addTilesetImage('ground', 'Planks');
-        let layers = groundFloor.createLayer(0, terrain, 0, 0);
+        let map = this.make.tilemap({ key: 'map' });
+        let terrain = map.addTilesetImage('planks', 'tiles');
+        let layers = map.createLayer(0, terrain, 0, 0);
     }
 }
+
+let config = {
+    type: Phaser.WEBGL,
+    width: 2560,
+    height: 1440,
+    backgroundColor: 'rgb(155, 16, 30)',
+    pixelArt: true, // Definetly never delete this, Upscaling an image so much with pixel interpolation enabled costs resources and looks horrifying
+    scene: {
+        preload: titleSceneClass.preload,
+        create: titleSceneClass.create,
+        update: titleSceneClass.update
+    }
+};
+
 let titleScene = new titleSceneClass();
 let game = new Phaser.Game(config);
 game.scene.add('titleScene', titleScene);
