87f8b4fa4a1f3718d74bdaf113011380ad10017d
Nick Pepper <nick.pepper@db-n.com>
2023-01-30 12:59:30 +0100

    Changed Resolution to fit 2k in F11 (Fullscreen Mode), Added CSS to Center Game on Website

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let config = {
     type: Phaser.WEBGL,
     backgroundColor: '#2dab2d',
-    width: 1920,
-    height: 1080,
-    pixelArt: true,
+    width: 2560,
+    height: 1440,
+    pixelArt: true, // Definetly never delete this, Upscaling an image so much with pixel interpolation enabled costs resources and looks horrifying
 };
 
 class titleSceneClass extends Phaser.Scene {
@@ -12,14 +12,14 @@ class titleSceneClass extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('bg', './assets/exported-images/backgroundCave.png');
+        this.load.image('bg', './assets/exported-images/backgroundCave.png'); // For further calculations relative to the game resolution: original image dimensions are 256x144 (10th of 2K)
         this.load.image('tilemap', './assets/exported-images/cPathTiles.png'); //Load resources for the tilemap JSON (Definetly load before dbnF1.json)
         this.load.tilemapTiledJSON('floor-one', './assets/exported-tilemaps/dbnF1.json'); //Load the tilemap
     }
 
     create () {
         let background = this.add.image(0, 0, 'bg').setOrigin(0);
-        background.setScale(7.5);
+        background.setScale(10); // If the resolution is set to 1920x1080px (FHD) then setScale(7.5), if set to 2560x1440 (2K QHD) then setScale(10)
     }
 }
 let titleScene = new titleSceneClass();
