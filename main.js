6f930a6fed9d379af37bd6f7a358bea0201933ac
Nick Pepper <nick.pepper@db-n.com>
2023-01-30 12:14:08 +0100

    Setup for tilemap loading done

--- main.js
+++ main.js
@@ -13,14 +13,16 @@ class titleSceneClass extends Phaser.Scene {
 
     preload() {
         this.load.image('bg', './assets/exported-images/backgroundCave.png');
+        this.load.image('tilemap', './assets/exported-images/cPathTiles.png'); //Load resources for the tilemap JSON (Definetly load before dbnF1.json)
+        this.load.tilemaptiledJSON('floor-one', './assets/exported-tilemaps/dbnF1.json'); //Load the tilemap
     }
 
     create () {
         let background = this.add.image(0, 0, 'bg').setOrigin(0);
-        background.setScale(7.5)
+        background.setScale(7.5);
     }
 }
 let titleScene = new titleSceneClass();
 let game = new Phaser.Game(config);
 game.scene.add('titleScene', titleScene);
-game.scene.start('titleScene')
+game.scene.start('titleScene');
