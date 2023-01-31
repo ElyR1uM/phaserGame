4cbcf72dd6d405911a19247b8f381e56cd21a2cd
Nick Pepper <nick.pepper@db-n.com>
2023-01-31 11:10:09 +0100

    Cleaned up some file names in assets/exported-images, added player Layer in Tiles

--- assets/tiled/Planks.tsx
+++ assets/tiled/Planks.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="Planks" tilewidth="16" tileheight="16" tilecount="81" columns="9">
- <image source="../exported-images/cPathTiles.png" width="144" height="144"/>
+ <image source="../exported-images/plankTiles.png" width="144" height="144"/>
 </tileset>
--- main.js
+++ main.js
@@ -6,17 +6,21 @@ class titleSceneClass extends Phaser.Scene {
 
     preload () {
         this.load.image('bg', 'assets/exported-images/backgroundCave.png'); // For further calculations relative to the game resolution: original image dimensions are 256x144 (10th of 2K)
-        this.load.image('tiles', 'assets/exported-images/cPathTiles.png'); //Load resources for the tilemap JSON (Definetly load before dbnF1.json)
+        this.load.image('plankTiles', 'assets/exported-images/plankTiles.png'); //Load resources for the tilemap JSON (Definetly load before dbnF1.json)
+        this.load.image('playerTiles', 'assets/exported-images/player.png');
         this.load.tilemapTiledJSON('map', 'assets/exported-tilemaps/dbnF1.json'); //Load the tilemap
     }
 
     create () {
         const map = this.make.tilemap({ key: 'map' });
-        const Planks = map.addTilesetImage('Planks', 'tiles'); //                           As a String                                     Not a string (duh)
-        const layers = map.createLayer('ground', Planks, 0, 0).setScale(5); //Layer Order is: '<What you called your layer in Tiled/The JSON>', <what you just defined as the TilesetImage (const Planks in line 18), 0, 0 (Coordinate Offset)
+        const Player = map.addTilesetImage('Player', 'playerTiles')
+        const Planks = map.addTilesetImage('Planks', 'plankTiles'); //                           As a String                                     Not a string (duh)
+        const groundLayer = map.createLayer('ground', Planks, 0, 0).setScale(5); //Layer Order is: '<What you called your layer in Tiled/The JSON>', <what you just defined as the TilesetImage (const Planks in line 18), 0, 0 (Coordinate Offset)
+        const playerLayer = map.createLayer('player')
 
-        //const overlay = this.add.image(0, 0, 'bg').setOrigin(0);
-        //overlay.setScale(10); // If the resolution is set to 1920x1080px (FHD) then setScale(7.5), if set to 2560x1440 (2K QHD) then setScale(10)
+
+        const overlay = this.add.image(0, 0, 'bg').setOrigin(0);
+        overlay.setScale(10); // If the resolution is set to 1920x1080px (FHD) then setScale(7.5), if set to 2560x1440 (2K QHD) then setScale(10)
     }
 }
 
