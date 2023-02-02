0634ccd3a35399a190c84c210790113e0ad67b2c
Nick Pepper <nick.pepper@db-n.com>
2023-02-02 10:42:09 +0100

    Added Matter.js back locally and added a <script> tag in the index to properly install the plugin

--- main.js
+++ main.js
@@ -11,9 +11,8 @@ class titleSceneClass extends Phaser.Scene {
 
     create () { // Create is called on game startup
         // Variables
-        let cursors;
         let scale = 5;
-        let playerBody = matter.Body;
+        let playerBody = Matter.Body;
         // Player Velocity
         let velocityX;
         let velocityY;
@@ -27,12 +26,12 @@ class titleSceneClass extends Phaser.Scene {
         //let playerLayer = map.createLayer('player', Player, 0, 0).setScale(scale); // unsure what value to put if the game is rendering in FHD -> To do: Calculate World size for FHD mode
         const Planks = map.addTilesetImage('Planks', 'plankTiles'); //                          VV As a String VV                                                       VV Not a string (duh) VV
         const groundLayer = map.createLayer('ground', Planks, 0, 0).setScale(scale); //createLayer Order is: '<What you called your layer in Tiled/The JSON>', <wh at you just defined as the TilesetImage (const Planks in line 18), 0, 0 (Coordinate Offset)
-        let playerChar = this.matter.add.sprite(104 * scale, 72 * scale, 'playerFrames')/*.setOrigin(0)*/.setScale(scale); // How to translate the 16x16 grid of the tilemap into the WQHD Canvas of the game: (N - .5, -1 if you have setOrigin to 0) Tiles times the scale var.
-
-        cursors = this.input.keyboard.createCursorKeys();
+        let playerChar = this.add.sprite(104 * scale, 72 * scale, 'playerFrames')/*.setOrigin(0)*/.setScale(scale); // How to translate the 16x16 grid of the tilemap into the WQHD Canvas of the game: (N - .5, -1 if you have setOrigin to 0) Tiles times the scale var.
         //const overlay = this.add.image(0, 0, 'bg').setOrigin(0); // Insert last to render above all
         //overlay.setScale(10); // If the resolution is set to 1920x1080px (FHD) then setScale(7.5), if set to 2560x1440 (2K QHD) then setScale(10)
     }
+
+    cursors = this.input.keyboard.createCursorKeys();
 
     inputHandler () {
         if (cursors.down.isDown) {
@@ -40,7 +39,6 @@ class titleSceneClass extends Phaser.Scene {
             // IMPORTANT NOTE: Matter physics has a function to apply force to a rigidbody2d on a vector2 coordinate system, which may be useful to use if coding movement with accel and decel
         }
     }
-
     update () { // Update is called once per frame, wonder if fixedUpdate is also a thing here
         this.inputHandler();
     }
