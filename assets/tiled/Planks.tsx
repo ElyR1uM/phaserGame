27103937ee467e4e6d53b013fb1286919b75e711
Nick Pepper <nick.pepper@db-n.com>
2023-02-02 17:02:05 +0100

    Various changes, such as an npc added, a new walls layer added, the spawn pos changed and now, there is collision with the walls and the npc. Sped up player movement

--- assets/tiled/Planks.tsx
+++ assets/tiled/Planks.tsx
@@ -1,4 +1,204 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Planks" tilewidth="16" tileheight="16" tilecount="81" columns="9">
- <image source="../exported-images/plankTiles.png" width="144" height="144"/>
+<tileset version="1.9" tiledversion="1.9.2" name="Planks" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="../exported-images/plankTiles.png" width="256" height="256"/>
+ <tile id="133">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="134">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="135">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="136">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="144">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="145">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="146">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="147">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="148">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="149">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="150">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="151">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="152">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="160">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="161">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="162">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="163">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="164">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="165">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="166">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="167">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="168">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="176">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="177">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="178">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="179">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="180">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="181">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="182">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="183">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="184">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="192">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="193">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="194">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="195">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="196">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="197">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="198">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="199">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="200">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const config = {
     type: Phaser.WEBGL,
     width: 2560,
     height: 1440,
-    backgroundColor: '#000021',
+    backgroundColor: '#212529', //#000021 is a more blue alternative + company colour, maybe use this if I get time to redo the walls' colour scheme
     pixelArt: true, // Definetly never delete this, Upscaling an image so much with pixel interpolation enabled costs resources and looks horrifying
     scene: {
         preload: preload,
@@ -32,6 +32,7 @@ function preload () { // Preload is called before startup
     this.load.image('bg', 'assets/exported-images/backgroundCave.png'); // For further calculations relative to the game resolution: original image dimensions are 256x144 (10th of 2K)
     this.load.image('plankTiles', 'assets/exported-images/plankTiles.png'); //Load resources for the tilemap JSON (Definetly load before dbnF1.json)
     this.load.spritesheet('playerFrames', 'assets/exported-images/player.png', {frameWidth: 16, frameHeight: 16});
+    this.load.spritesheet('npcFrames', 'assets/exported-images/npc0.png', {frameWidth: 16, frameHeight: 16});
     this.load.tilemapTiledJSON('map', 'assets/exported-tilemaps/dbnF1.json'); //Load the tilemap
 }
 
@@ -45,13 +46,14 @@ function create () { // Create is called on game startup
     const map = this.make.tilemap({ key: 'map' });
     const Planks = map.addTilesetImage('Planks', 'plankTiles');
     for (let i = 0; i < map.layers.length; i++) {
-      const layer = map.createLayer(i, Planks, 0, 0).setScale(scale)
+      const layer = map.createLayer(i, Planks, 0, 0).setScale(scale);
       layer.setDepth(i);
     }
     playerChar = this.add.sprite(0, 0, 'playerFrames').setScale(scale);
     playerChar.setDepth(2);
     this.cameras.main.startFollow(playerChar, true);
     this.cameras.main.setFollowOffset(-playerChar.width, -playerChar.height);
+    const npcSprite = this.add.sprite(0, 0, "npcFrames").setScale(scale);
     //playerChar.setColliderWorldBounds(true);
     //const overlay = this.add.image(0, 0, 'bg').setOrigin(0); // Insert last to render above all
     //overlay.setScale(10); // If the resolution is set to 1920x1080px (FHD) then setScale(7.5), if set to 2560x1440 (2K QHD) then setScale(10)
@@ -60,34 +62,19 @@ function create () { // Create is called on game startup
           {
             id: 'player',
             sprite: playerChar,
-            startPosition: { x: 6, y: 4 },
+            startPosition: { x: 31, y: 18 },
+            speed: 8,
+          },
+          {
+            id: "npc0",
+            sprite: npcSprite,
+            startPosition: { x: 39, y: 7 },
           },
         ],
     }
 
     this.gridEngine.create(map, gridEngineConfig);
 }
-
-/*function inputHandler () {
-    if (cursors.left.isDown) {
-        playerChar.setVelocityX(-200);
-    } 
-    if (cursors.right.isDown) {
-        playerChar.setVelocityX(200);
-    }
-    if (!cursors.left.isDown && !cursors.right.isDown || cursors.left.isDown && cursors.right.isDown) {
-        playerChar.setVelocityX(0);
-    }
-    if (cursors.up.isDown) {
-        playerChar.setVelocityY(-200);
-    }
-    if (cursors.down.isDown) {
-        playerChar.setVelocityY(200);
-    }
-    if (!cursors.up.isDown && !cursors.down.isDown || cursors.up.isDown && cursors.down.isDown) {
-        playerChar.setVelocityY(0);
-    }
-}*/
 
 function update () { // Update is called once per frame, wonder if fixedUpdate is also a thing here
     //inputHandler();
